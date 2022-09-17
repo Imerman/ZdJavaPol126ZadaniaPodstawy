e95a3e8d9cbd356d8e2ed5aaf5deaed1b356c7f3
Dupa <dupagoryla@abc.pl>
2022-09-17 12:18:29 +0200

    task2

--- src/pl/sdacademy/java/basic/exercises/day1/Task2.java
+++ src/pl/sdacademy/java/basic/exercises/day1/Task2.java
@@ -1,2 +1,34 @@
-package pl.sdacademy.java.basic.exercises.day1;public class Task2 {
+package pl.sdacademy.java.basic.exercises.day1;
+
+import java.util.Scanner;
+
+public class Task2 {
+    private static final float PI = 3.14f;
+    public static void main(String[] args) {
+        float radius = getDiameterFromUser() / 2;
+        System.out.println("radius value: " + radius);
+        float circumference = calculateCircumferenceOfACircle(radius);
+        float area = calculateAreaOfACircle(radius);
+        System.out.println("Circumference: " + circumference);
+        System.out.println("Area: " + area);
+
+    }
+    private static float getDiameterFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please Insert Diameter: ");
+//        float diameter = scanner.nextFloat();
+//        return diameter;
+        return scanner.nextFloat();
+    }
+
+    private static float calculateCircumferenceOfACircle (float radius){
+//        2 * pi * r;
+//        return 2 * PI * radius;
+        return 2 * (float) Math.PI * radius;
+    }
+
+    private static float calculateAreaOfACircle(float radius){
+//        return PI *radius * radius;
+        return (float) ( Math.PI * Math.pow(radius, 2));
+    }
 }
---
+++ src/pl/sdacademy/java/basic/exercises/day1/Task4.java
@@ -0,0 +1,4 @@
+package pl.sdacademy.java.basic.exercises.day1;
+
+public class Task4 {
+}
