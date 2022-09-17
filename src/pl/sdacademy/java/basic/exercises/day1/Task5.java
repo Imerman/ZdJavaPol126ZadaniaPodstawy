5d727483733a79b4b15289a2004e0a3f3ed33b0e
Dupa <dupagoryla@abc.pl>
2022-09-17 14:23:36 +0200

    add task5

---
+++ src/pl/sdacademy/java/basic/exercises/day1/Task5.java
@@ -0,0 +1,29 @@
+package pl.sdacademy.java.basic.exercises.day1;
+
+public class Task5 {
+    public static void main(String[] args) {
+        int firstDigit =4;
+        int secondDigit = 11;
+        int sum = sumOfTheSequence(firstDigit, secondDigit);
+        System.out.println("Sum: " + sum);
+    }
+    private static int sumOfTheSequence (int firstDigit, int secondDigit) {
+        int sum = 0;
+//        #1 metoda
+//        for (int i = firstDigit; i <= secondDigit; i++) {
+//            sum += i;
+//        }
+//        #2 metoda
+//        while (firstDigit <= secondDigit){
+//            sum +=firstDigit;
+//            firstDigit++;
+//        }
+//        #3 metoda
+//        do{
+//
+//
+//        } while ();
+//
+//        return sum;
+    }
+}
